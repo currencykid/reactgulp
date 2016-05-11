d2c54a4119d58fb6f252c7f9ed74a7a3f909a3e1
ck <thecurrencykid@gmail.com>
2016-05-11 14:18:29 -0700

    about to start 14.tooling- applying NPM

---
+++ thumbnail-gulp/src/app.jsx
@@ -0,0 +1,28 @@
+var options = {
+thumbnailData: [{
+title: "See tutorials",
+number: "69" , 
+header: "Learn React",
+description: "React is a new library for making awesome and dynamic pages",
+imageUrl: 'https://facebook.github.io/react/img/logo.svg' 
+}, {
+title: "See tutorials",
+number: "19" , 
+header: "Learn Gulp",
+description: "Gulp is a buildsystem that helps you get all the shit you need for React together.",
+imageUrl: 'https://avatars0.githubusercontent.com/u/6200624?v=3&s=400' 
+}]
+}; 
+
+//ask react to render the class
+// this is essentially the cutout from the "cookie cutter" 
+// pass the object 'options'as second argument
+
+var element = React.createElement(ThumbnailList, options); 
+
+
+// when we ask react to render, 
+//we tell it where to place the rendered elemtn in the DOM
+// first artguemnt is isntance,, the second is where we want to render to
+
+  ReactDOM.render(element, document.querySelector('.container')); 
---
+++ thumbnail-gulp/src/thumbnail-list.jsx
@@ -0,0 +1,16 @@
+
+//gonna use map on an array to call a function on every element in the array
+var ThumbnailList = React.createClass({
+  render: function(){
+    var list = this.props.thumbnailData.map(function(thumbnailProps){
+    // the following is a React shortcut for passing in an entire object
+    // take entire thumbnailProps options object and create instance of thumbanil and pass in all properties
+    // list is now an array of thumbnail instance
+      return <Thumbnail {...thumbnailProps} /> 
+}); 
+  return <div> 
+  {list} 
+  </div>
+}
+});
+
